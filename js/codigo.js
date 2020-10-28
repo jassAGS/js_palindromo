54b46c80053c8bc4d2e76f3e916743f473b539be
jassAGS <45088485+jassAGS@users.noreply.github.com>
2020-10-27 20:49:26 -0600

    primer commit
    palindrome

---
+++ js/codigo.js
@@ -0,0 +1,20 @@
+var frase = prompt("ingrese la cadena:");
+
+function palindromo(frase){
+  var fraseusar= frase.split(' ').join('');
+  var fraseal ='';
+  var palindromesino='';
+  for(var i=fraseusar.length-1; i>=0; i-- ){
+    fraseal +=fraseusar[i];
+
+  }
+  if(fraseusar.toLowerCase()==fraseal.toLowerCase()){
+    alert("es palindromo");
+  }else{
+    alert("no es palindromo");
+  }
+
+  return palindromesino;
+}
+
+palindromo(frase);
